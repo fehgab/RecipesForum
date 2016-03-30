b882807c77463f3307f869ddbbcdd276d1363d61
FG <fgsboarder@gmail.com>
2016-03-30 22:02:56 +0200

    Added sort method by columns

--- RecipesClient/MainWindow.xaml.cs
+++ RecipesClient/MainWindow.xaml.cs
@@ -1,22 +1,15 @@
 ﻿using RecipesClient.DAL;
 using RecipesClient.DTO;
+using RecipesClient.WindowManagers;
 using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.SqlClient;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace RecipesClient
 {
@@ -25,14 +18,23 @@ namespace RecipesClient
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool lwRecipesLoaded = false;
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
+        private ResizeListViewColums resizeLwColumns = new ResizeListViewColums();
+
         public MainWindow()
         {
             InitializeComponent();
-            this.WindowState = WindowState.Maximized;
+            WindowState = WindowState.Maximized;
         }
 
         private void lwRecipes_Loaded(object sender, RoutedEventArgs e)
-        {     
+        {  
+               
             using (var db = new RecipesModel())
             {
                 var allRecipes = db.Recipes.ToList();
@@ -42,16 +44,49 @@ namespace RecipesClient
                     lwRecipes.Items.Add(new RecipesHeaderData { PictureFullPath = picturePath, Title = recipe.Title, Ingredients = recipe.Ingredients, PrepareTime = recipe.PrepareTime });
                 }
             }
+            lwRecipesLoaded = true;
         }
 
         private void lwRecipes_LayoutUpdated(object sender, EventArgs e)
         {
-            double lwSize = gvcPicture.ActualWidth + gvcTitle.ActualWidth + gvcPrepareTime.ActualWidth + gvcIngredients.ActualWidth;
-            double size = (this.ActualWidth - lwSize) / 2;
-            if(lwSize < this.ActualWidth)
+            if(lwRecipesLoaded)
             {
-                lwRecipes.Padding = new Thickness(size, 0, 0, 0);
-            }               
+                double lwSize = gvcPicture.ActualWidth + gvcTitle.ActualWidth + gvcPrepareTime.ActualWidth + gvcIngredients.ActualWidth;
+                double size = (this.ActualWidth - lwSize) / 2;
+                lwRecipes.Padding = new Thickness(0, 0, 0, 0);
+                if (lwSize < this.ActualWidth)
+                {
+                    lwRecipes.Padding = new Thickness(size, 0, 0, 0);
+                }
+                resizeLwColumns.SetMinColumsWidth(this);
+            }
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                GridViewColumnHeader column = (sender as GridViewColumnHeader);
+                string sortBy = column.Tag.ToString();
+                if (listViewSortCol != null)
+                {
+                    AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                    lwRecipes.Items.SortDescriptions.Clear();
+                }
+
+                ListSortDirection newDir = ListSortDirection.Ascending;
+                if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                    newDir = ListSortDirection.Descending;
+
+                listViewSortCol = column;
+                listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+                lwRecipes.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+            }
+        }
+
+        private void GridViewColumnHeader_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            gvcPicture.Width = gvcPicture.ActualWidth;
         }
     }
 }
---
+++ RecipesClient/WindowManagers/ResizeListViewColums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesClient.WindowManagers
+{
+    class ResizeListViewColums
+    {
+        public void SetMinColumsWidth(MainWindow mw)
+        {
+            if(mw.gvcPicture.ActualWidth < 200)
+                mw.gvcPicture.Width = 200;
+            if (mw.gvcTitle.ActualWidth < 100)
+                mw.gvcTitle.Width = 100;
+            if (mw.gvcPrepareTime.ActualWidth < 120)
+                mw.gvcPrepareTime.Width = 120;
+            if (mw.gvcIngredients.ActualWidth < 110)
+                mw.gvcIngredients.Width = 110;
+        }
+    }
+}
